0dde1ce539c46fea72ffc36b68bbd3bd29df52c0
Yogender <129525898+yogender747@users.noreply.github.com>
2024-10-14 02:09:08 +0530

    Update sharpx.py

--- sharpx.py
+++ sharpx.py
@@ -2,13 +2,13 @@
 #SCRIPT BY SharpX72
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("8172562388:AAHn2lVZPlvob24Za4MaNL9m_3TBxLJC21c")
+BOT_TOKEN=("521102340:AAGBMzvT1yKHH4xxbKMdItxGFTWEX_eUHN4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("SharpX72")
+OWNER_USERNAME=("Rishi747")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1009132250")
+ADMIN_IDS=("1319994892")
 
 
 

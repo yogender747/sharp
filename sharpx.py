bf280ef3d710e2dc38a50c10f6c1bda354fb21e4
Yogender <129525898+yogender747@users.noreply.github.com>
2024-10-14 02:06:09 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8172562388:AAHn2lVZPlvob24Za4MaNL9m_3TBxLJC21c")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("SharpX72")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1009132250")
+
+
+
+
+
+  
